3d155d360b540f115d5f15a34d57c1d3155238cc
ono <harinezumi65536@gmail.com>
2022-04-24 20:41:36 +0900

    ログイン機能を実装

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'amended_return.dart';
@@ -12,7 +13,7 @@ import 'approval.dart';
 import 'history.dart';
 
 // 異なるページで共通して使う変数を定義
-final UserName = Provider((ref) {});
+final UserName = StateProvider((ref) => '');
 // 打刻画面の情報を記録し,画面が遷移しても打刻が記録できるよう定義
 final AttendTime = StateProvider<String>((ref) => '');
 final LeaveTime = StateProvider((ref) => '');
@@ -24,6 +25,7 @@ final Create_at =
 final Working = StateProvider<bool>((ref) => false);
 final Resting = StateProvider<bool>((ref) => false);
 final Status = StateProvider<String>((ref) => '');
+final Admin = StateProvider<bool>((ref) => false);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -43,21 +45,132 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: const MainPage(),
+      theme: ThemeData.light().copyWith(
+        textTheme: GoogleFonts.sawarabiGothicTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        textTheme: GoogleFonts.sawarabiGothicTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
+      ),
+      home: const LoginPage(),
     );
   }
 }
 
-class MainPage extends StatelessWidget {
+class LoginPage extends ConsumerStatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  // メッセージ表示用
+  String infoText = '';
+  // 入力したメールアドレス・パスワード
+  String email = '';
+  String password = '';
+  String user_name = '';
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    // ユーザー名の保存
+    final user = ref.watch(UserName.notifier);
+    final admin = ref.watch(Admin.notifier);
+
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // メールアドレス入力
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'ユーザー名'),
+                onChanged: (String value) {
+                  setState(() {
+                    user_name = value;
+                  });
+                },
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                // メッセージ表示
+                child: Text(infoText),
+              ),
+              SizedBox(
+                width: double.infinity,
+                // ユーザーログインボタン
+                child: ElevatedButton(
+                  child: const Text('ユーザーログイン'),
+                  onPressed: user_name == ''
+                      ? null
+                      : () async {
+                          user.state = user_name;
+                          await Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) {
+                              return const MainPage();
+                            }),
+                          );
+                        },
+                ),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                width: double.infinity,
+                // ログイン登録ボタン
+                child: ElevatedButton(
+                  child: const Text('管理者ログイン'),
+                  onPressed: user_name == ''
+                      ? null
+                      : () async {
+                          user.state = user_name;
+                          admin.state = true;
+                          await Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) {
+                              return const MainPage();
+                            }),
+                          );
+                        },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends ConsumerWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final admin = ref.watch(Admin.notifier);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('ホーム画面'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              // ログイン画面に遷移＋ホーム画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return const LoginPage();
+                }),
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -67,7 +180,12 @@ class MainPage extends StatelessWidget {
               width: 128,
               height: 64,
               child: ElevatedButton(
-                child: const Text('履歴'),
+                child: const Text(
+                  '勤務履歴',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
                 // 画面遷移のボタンイベント
                 onPressed: () => Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) {
@@ -80,7 +198,12 @@ class MainPage extends StatelessWidget {
               width: 128,
               height: 64,
               child: ElevatedButton(
-                child: const Text('打刻'),
+                child: const Text(
+                  '打刻',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
                 // 画面遷移のボタンイベント
                 onPressed: () => Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) {
@@ -93,7 +216,12 @@ class MainPage extends StatelessWidget {
               width: 128,
               height: 64,
               child: ElevatedButton(
-                child: const Text('申請'),
+                child: const Text(
+                  '申請',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
                 // 画面遷移のボタンイベント
                 onPressed: () => Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) {
@@ -105,14 +233,21 @@ class MainPage extends StatelessWidget {
             SizedBox(
               width: 128,
               height: 64,
-              child: ElevatedButton(
-                child: const Text('打刻修正の承認'),
-                // 画面遷移のボタンイベント
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return const ApprovalPage();
-                })),
-              ),
+              child: Visibility(
+                  child: ElevatedButton(
+                    child: const Text(
+                      '修正の承認',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    // 画面遷移のボタンイベント
+                    onPressed: () => Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return const ApprovalPage();
+                    })),
+                  ),
+                  visible: admin.state),
             ),
           ],
         ),
